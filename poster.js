d3eba39bcf6a1d79b3b86c2fa77f83fe268c761c
Maximaaal <maxrentmeester@live.nl>
2022-12-28 16:56:17 +0100

    instant results

--- poster.js
+++ poster.js
@@ -13,11 +13,15 @@ document.getElementById('bg').style.background = "linear-gradient(-200deg, " + s
 window.onload = loadInfo()
 
 function loadInfo() {
-    var storedResult = JSON.parse(localStorage.getItem("result"))
+    var storedResult = (localStorage.getItem("result"))
     var storedTitle = (localStorage.getItem("title"))
     var storedImg = (localStorage.getItem("img"))
     var storedGenre = (localStorage.getItem("genre"))
+    console.log(localStorage.getItem('storedLoadingStatus'))
     console.log(storedResult)
+    console.log(storedTitle)
+    console.log(storedImg)
+    console.log(storedGenre)
     document.getElementById('movieTitle').innerHTML = storedTitle
     document.getElementById('moviePoster').src = storedImg
     document.getElementById('genre').innerHTML = storedGenre
--- results.js
+++ results.js
@@ -1,50 +1,55 @@
 var storedColors = JSON.parse(localStorage.getItem("colors"));
 document.getElementById('bg').style.background = "linear-gradient(-200deg, " + storedColors[0]+ " 0%, " + storedColors[1]+" 100%)"
 
-var movieName = []
-var storedPosterName = []
-var retrievedName = []
+function put_object(obj){
+    var finishedLoading = obj.get("loadingStatus")
+    if (finishedLoading == 1){
+        console.log("finished loading")
+        showResults()
+    }
+}
 
-
-
-// function loadInfo() {
-//     for (let i = 1; i < 1; i++) {
-//         movieName[i] = document.getElementById('poster' + i).getAttribute("data-name")
-//         localStorage.setItem('posterName' + i, JSON.stringify(retrievedName[i]))
-//         storedPosterName[i] = JSON.parse(localStorage.getItem("posterName" + i));
-//         console.log(storedPosterName[i])
-//     }
-// }
-
-
-var name1 = document.getElementById('poster1').getAttribute("data-name")
-localStorage.setItem('posterName1', JSON.stringify(name1))
-var storedPosterName1 = JSON.parse(localStorage.getItem("posterName1"));
-console.log(storedPosterName1)
-
-var year = document.getElementById('poster1').getAttribute("data-year")
-localStorage.setItem('posterYear', JSON.stringify(year))
-var storedPosterYear = JSON.parse(localStorage.getItem("posterYear"));
-console.log(storedPosterYear)
+var storedResult
+var storedTitle
+var storedImg
+var storedGenre
 
 function showResults(){
-    for (let i = 0; i < 17; i++) {
+    for (let i = 0; i < 16; i++) {
         console.log(`${pyscript.runtime.globals.get('output' + i)}`)
         document.getElementById('poster' + i).src = `${pyscript.runtime.globals.get('output' + i)}`
-        document.getElementById('poster' + i).addEventListener('click', function() {
-            localStorage.setItem('result',(i))
+            localStorage.setItem('result' + i,(i))
             localStorage.setItem('title', `${pyscript.runtime.globals.get('output' + i + 'title')}`)
             localStorage.setItem('img', `${pyscript.runtime.globals.get('output' + i)}`)
             localStorage.setItem('genre', `${pyscript.runtime.globals.get('output' + i + 'genre')}`)
-            var storedResult = (localStorage.getItem("result"))
-            var storedTitle = (localStorage.getItem("title"))
-            var storedImg = (localStorage.getItem("img"))
-            var storedGenre = (localStorage.getItem("genre"))
+            storedResult = (localStorage.getItem("result"))
+            storedTitle = (localStorage.getItem("title"))
+            storedImg = (localStorage.getItem("img"))
+            storedGenre = (localStorage.getItem("genre"))
             console.log(storedResult)
             console.log(storedTitle)
             console.log(storedImg)
             console.log(storedGenre)
-        })
     }
 }
 
+// function showResults(){
+//     for (let i = 0; i < 17; i++) {
+//         console.log(`${pyscript.runtime.globals.get('output' + i)}`)
+//         document.getElementById('poster' + i).src = `${pyscript.runtime.globals.get('output' + i)}`
+//         document.getElementById('poster' + i).addEventListener('click', function() {
+//             localStorage.setItem('result',(i))
+//             localStorage.setItem('title', `${pyscript.runtime.globals.get('output' + i + 'title')}`)
+//             localStorage.setItem('img', `${pyscript.runtime.globals.get('output' + i)}`)
+//             localStorage.setItem('genre', `${pyscript.runtime.globals.get('output' + i + 'genre')}`)
+//             var storedResult = (localStorage.getItem("result"))
+//             var storedTitle = (localStorage.getItem("title"))
+//             var storedImg = (localStorage.getItem("img"))
+//             var storedGenre = (localStorage.getItem("genre"))
+//             console.log(storedResult)
+//             console.log(storedTitle)
+//             console.log(storedImg)
+//             console.log(storedGenre)
+//         })
+//     }
+// }
